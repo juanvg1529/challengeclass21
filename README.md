84508d855e2e1ab153070cbdfe34ea588ccc4bbd
juanvg1529 <rockjdvg1529@gmail.com>
2021-04-15 17:00:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# challengeclass21
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/challengeclass21)
---
+++ script.js
@@ -0,0 +1,16 @@
+//Write a cycle that shows at the console the numbers from 1 to 10
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`The number is: ${i}`);
+}
+//Write a cycle that shows at the console the numbers from 10 to 1
+for (let i = 10; i >= 1; i--) {
+  console.log(`The number is: ${i}`);
+}
+
+for (let i = 0; i <= 30; i++) {
+  if (i % 2 === 0) {
+    // The module of a even number is always 0
+    console.log(`emparedado`);
+  }
+}
